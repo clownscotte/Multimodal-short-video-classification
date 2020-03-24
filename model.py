c7787aacd191166551731c50392d4fac82ba75a0
clownscotte <980105774@qq.com>
2020-03-24 18:47:34 +0800

    Update model.py

--- model.py
+++ model.py
@@ -2,7 +2,9 @@ import torch
 from lmf import LMF
 from hmc import HMC
 import torch.nn as nn
+import data_extractor
 from pytorch_transformers import BertTokenizer
+
 
 class VideoClassificationModel(nn.Module):
     def __init__(self, L1_labels_num, L2_labels_num, L12_table):
@@ -14,7 +16,7 @@ class VideoClassificationModel(nn.Module):
         self.hmc = HMC(feature_size=1024, L1_labels_num=self.L1_labels_num, L2_labels_num=L2_labels_num, L12_table=self.L12_table)
 
     def forward(self, images, texts, tokenizer):
-        out_lmf = self.lmf(images, texts, tokenizer)
+        out_lmf = self.lmf(audio, images, texts, tokenizer)
         L1, L2 = self.hmc(out_lmf)
         return L1, L2
 
@@ -22,6 +24,14 @@ if __name__ == '__main__':
     images = torch.randn(1, 3, 299, 299)
     texts = ['[CLS] 你是谁? 你是好人. [SEP]']
     tokenizer = BertTokenizer.from_pretrained('bert-base-chinese')
+
+    texts=data_extractor.extract_video_Name(filename)
+    images=data_extractor.extract_video_Frames(filemane)
+    audio=data_extractor.video_to_audio(filename)
+
+
+
+
 
     model = VideoClassificationModel(5, 7, [[0], [2, 3, 4], [5], [6], [1]])
     model_image = model.lmf.inceptionresnetv2
